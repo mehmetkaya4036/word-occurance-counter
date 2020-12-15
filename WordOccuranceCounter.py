e6af15f1ba6fc508b11b35456e03b9b03bd0029b
Komando <kitaplardanfikirler@gmail.com>
2020-12-15 16:16:24 +0100

    V02 five new function created

--- WordOccuranceCounter.py
+++ WordOccuranceCounter.py
@@ -2,68 +2,78 @@ import pandas as pd
 import string
 import matplotlib.pyplot as plt
 import numpy as np
-""" 
-Bug - because of punctuations there is misinfo about word counts. 
-to solve it in context variable, get rid of punctuations before spliting words
-regex can be helpful
-"""
-# subtitle opened and named life
-with open("IronManDutch.srt") as file:
-    # returned list seperated with \n
-    lines = file.read().splitlines()
-    print("checking first 10 element of list")
-    print(lines[:10])
 
-    # empty strings, numbers and times eliminated from list by creating new list
+
+def readSubtitleToList(filepath="IronManDutch.srt"):
+    """this function reads file and returns as list of sentances splited line by line"""
+    with open(filepath) as file:
+        return file.read().splitlines()
+
+
+def eliminateNumEmpStr(linesList):
+    """ empty strings, numbers and times eliminated from list by returning new list which has just sentances """
     justSentances = []
-    for line in lines:
-        if line == "":
+    for line in linesList:
+        if line == "" or line == " ":
             continue
         if line[0].isdigit():
             continue
         else:
             justSentances.append(line)
-    print("checking how many sentences do we have")
-    print(len(justSentances))
-    print("checking first 7 sentences")
-    print(justSentances[:7])
-
-    # Time to count words
-    # first combine list to seperate all text via space to take words
-    context = " ".join(justSentances).lower()
-    #eliminate punctuation
-    context = context.translate(str.maketrans("","",string.punctuation))
-    #make all lower case and split
-    words = context.split(sep=" ")
+    return justSentances
 
 
-    print("checking how many words do we have: " + str(len(words)))
+def listToLowercaseString(linesList, merger=" "):
+    """takes list of sentances makes them lowercase returns string merged with space"""
+    return merger.join(linesList).lower()
 
-    # Creating dictionary for words and their frequency
-    wordOccurance = {}
-    uniqueWords=set(words)
+
+def eliminatePunctuation(text):
+    """takes text returns text without punctuation """
+    return text.translate(str.maketrans("", "", string.punctuation))
+
+
+def wordFreqDic(wordsList):
+    """takes list of words and returns word:frequency dictionary"""
+    wordOccuranceDic = {}
+    uniqueWords = set(wordsList)
     print("checking how many unique Words do we have: " + str(len(uniqueWords)))
     for word in uniqueWords:
-        wordOccurance[word] = words.count(word)
-
-    print("wordOccurance len: " + str(len(wordOccurance)))
-
-    #turn dic ti pandas series to sorting and seeing head and writing as csv
-    df = pd.Series(wordOccurance)
-    values = df.sort_values(ascending=False)
-    print("dataframe len: " + str(len(df)))
-
-    print(values.head(10))
-
-    print(values.describe())
-
-    plt.scatter(values.get_values(), range(len(values.get_values())), s=values.get_values(), c=np.random.rand(1771), alpha=0.5)
-    plt.show()
-    # values.to_csv('out.csv')
-
-    # with open("words.txt","a") as words:
-    #     for item in justSentances:
-    #         words.write(item+"\n")
-    #     print("done")
+        wordOccuranceDic[word] = wordsList.count(word)
+    return wordOccuranceDic
 
 
+## part 1 ##
+# subtitle opened and returned list seperated with \n
+lines = readSubtitleToList(filepath="IronManDutch.srt")
+print("checking first 10 element of list: ", lines[:10])
+
+# empty strings, numbers and times eliminated from list by creating new list
+justSentances = eliminateNumEmpStr(lines)
+print("checking how many sentences do we have: ", len(justSentances))
+print("checking first 7 sentences: ", justSentances[:7])
+
+## part 2 ##    # Time to count words
+# first combine list to seperate all text via space to take words
+context = eliminatePunctuation(listToLowercaseString(justSentances, merger=" "))
+# make all lower case and split
+words = context.split(sep=" ")
+print("checking how many words do we have: " + str(len(words)))
+
+# Creating dictionary for words and their frequency
+# turn dic ti pandas series to sorting and seeing head and writing as csv
+df = pd.Series(wordFreqDic(words))
+values = df.sort_values(ascending=False)
+print("dataframe len: " + str(len(df)))
+print(values.head(10))
+print(values.describe())
+
+plt.scatter(values.get_values(), range(len(values.get_values())), s=values.get_values(), c=np.random.rand(len(values.get_values())),
+            alpha=0.5)
+plt.show()
+# values.to_csv('out.csv')
+
+# with open("words.txt","a") as words:
+#     for item in justSentances:
+#         words.write(item+"\n")
+#     print("done")
