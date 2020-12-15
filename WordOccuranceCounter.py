432efa515a907f4187f0ff42a6159cbb6ce4d122
Komando <kitaplardanfikirler@gmail.com>
2020-12-15 21:49:41 +0100

    V03

--- WordOccuranceCounter.py
+++ WordOccuranceCounter.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def readSubtitleToList(filepath="IronManDutch.srt"):
+def readSubtitleToList(filepath):
     """this function reads file and returns as list of sentances splited line by line"""
-    with open(filepath) as file:
+    with open(filepath, encoding="cp437") as file:
         return file.read().splitlines()
 
 
@@ -30,50 +30,70 @@ def listToLowercaseString(linesList, merger=" "):
 
 def eliminatePunctuation(text):
     """takes text returns text without punctuation """
+    getRidOf = ["<i>", "</i>"] + [str(i) for i in range(10)]
+    for unnecessaryPunch in getRidOf: text = text.replace(unnecessaryPunch, "")
     return text.translate(str.maketrans("", "", string.punctuation))
 
 
-def wordFreqDic(wordsList):
+def wordFreqDic(wordsList, report=True):
     """takes list of words and returns word:frequency dictionary"""
     wordOccuranceDic = {}
-    uniqueWords = set(wordsList)
-    print("checking how many unique Words do we have: " + str(len(uniqueWords)))
+    uniqueWords = set(wordsList) - {""}
+    if report:
+        print("checking how many unique Words do we have: " + str(len(uniqueWords)))
     for word in uniqueWords:
         wordOccuranceDic[word] = wordsList.count(word)
     return wordOccuranceDic
 
 
-## part 1 ##
-# subtitle opened and returned list seperated with \n
-lines = readSubtitleToList(filepath="IronManDutch.srt")
-print("checking first 10 element of list: ", lines[:10])
+def txtTofreqDic(filepath, report=True):
+    """takes filePath of subtitle-.srt- or .txt strip from punctuation, time, digits and
+    returns a dictionary of word:occuranceFrewuency"""
+    linesAsList = readSubtitleToList(filepath)
+    linesAsListWithoutNumbers = eliminateNumEmpStr(linesAsList)
+    text = eliminatePunctuation(listToLowercaseString(linesAsListWithoutNumbers))
+    textToWordsAsList = text.split(sep=" ")
 
-# empty strings, numbers and times eliminated from list by creating new list
-justSentances = eliminateNumEmpStr(lines)
-print("checking how many sentences do we have: ", len(justSentances))
-print("checking first 7 sentences: ", justSentances[:7])
+    if report:
+        print("checking how many words do we have: " + str(len(textToWordsAsList)))
 
-## part 2 ##    # Time to count words
-# first combine list to seperate all text via space to take words
-context = eliminatePunctuation(listToLowercaseString(justSentances, merger=" "))
-# make all lower case and split
-words = context.split(sep=" ")
-print("checking how many words do we have: " + str(len(words)))
+    return wordFreqDic(textToWordsAsList, report)
 
-# Creating dictionary for words and their frequency
-# turn dic ti pandas series to sorting and seeing head and writing as csv
-df = pd.Series(wordFreqDic(words))
-values = df.sort_values(ascending=False)
-print("dataframe len: " + str(len(df)))
-print(values.head(10))
-print(values.describe())
 
-plt.scatter(values.get_values(), range(len(values.get_values())), s=values.get_values(), c=np.random.rand(len(values.get_values())),
-            alpha=0.5)
-plt.show()
-# values.to_csv('out.csv')
+if __name__ == "__main__":
+    ## part 1 ##
+    # subtitle opened and returned list seperated with \n
+    lines = readSubtitleToList(filepath="TurboDutch.srt")
+    print("checking first 10 element of list: ", lines[:10])
 
-# with open("words.txt","a") as words:
-#     for item in justSentances:
-#         words.write(item+"\n")
-#     print("done")
+    # empty strings, numbers and times eliminated from list by creating new list
+    justSentances = eliminateNumEmpStr(lines)
+    print("checking how many sentences do we have: ", len(justSentances))
+    print("checking first 7 sentences: ", justSentances[:7])
+
+    ## part 2 ##    # Time to count words
+    # first combine list to seperate all text via space to take words
+    context = eliminatePunctuation(listToLowercaseString(justSentances, merger=" "))
+    # make all lower case and split
+    words = context.split(sep=" ")
+    print("checking how many words do we have: " + str(len(words)))
+    print("checking first 10 element of list: ", words[:20])
+
+    # Creating dictionary for words and their frequency
+    # turn dic ti pandas series to sorting and seeing head and writing as csv
+    df = pd.Series(wordFreqDic(words))
+    values = df.sort_values(ascending=False)
+    print("dataframe len: " + str(len(df)))
+    print(values.head(10))
+    print(values.describe())
+
+    plt.scatter(values.get_values(), range(len(values.get_values())), s=values.get_values(),
+                c=np.random.rand(len(values.get_values())),
+                alpha=0.5)
+    plt.show()
+    # values.to_csv('out.csv')
+
+    # with open("words.txt","a") as words:
+    #     for item in justSentances:
+    #         words.write(item+"\n")
+    #     print("done")
